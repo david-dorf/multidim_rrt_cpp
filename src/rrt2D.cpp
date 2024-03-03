7db40d8ad3d0a8d261fce71de32475f56994ad94
daviddorf2023 <daviddorf2023@u.northwestern.edu>
2024-03-03 16:40:34 -0500

    RRT publishes a path correctly, once

--- src/rrt2D.cpp
+++ src/rrt2D.cpp
@@ -11,7 +11,7 @@ public:
         wall_confidence(50)
   {
     path_publisher = create_publisher<nav_msgs::msg::Path>("path", 10);
-    runRRT();
+    run_rrt();
   }
 
 private:
@@ -29,17 +29,33 @@ private:
   std::vector<float> goal_position = {goal_x, goal_y};
   std::vector<int> map_size = {100, 100};
   TreeNode *root_node = new TreeNode(start_position, nullptr);
-  TreeNode *goal_node = new TreeNode(goal_position, nullptr);
   std::vector<TreeNode *> node_list = {root_node};
+  nav_msgs::msg::Path path;
   rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr path_publisher;
 
-  void runRRT()
+  void publish_path()
+  {
+    path.header.frame_id = "map";
+    path.header.stamp = get_clock()->now();
+    TreeNode *current_node = node_list.back();
+    while (current_node->parent != nullptr)
+    {
+      geometry_msgs::msg::PoseStamped pose;
+      pose.pose.position.x = current_node->val[0];
+      pose.pose.position.y = current_node->val[1];
+      path.poses.push_back(pose);
+      current_node = current_node->parent;
+    }
+    path_publisher->publish(path);
+  }
+
+  void run_rrt()
   {
     RCLCPP_INFO(get_logger(), "Running RRT");
     while (node_list.size() < node_limit)
     {
-      float random_position_x = rand() % map_size[0];
-      float random_position_y = rand() % map_size[1];
+      const float random_position_x = rand() % map_size[0];
+      const float random_position_y = rand() % map_size[1];
       float min_distance = INFINITY;
       std::vector<float> random_position = {random_position_x, random_position_y};
       std::vector<float> min_node_vec;
@@ -51,9 +67,11 @@ private:
         const float goal_distance = sqrt(pow(goal_vec[0], 2) + pow(goal_vec[1], 2));
         if (goal_distance < goal_tolerance)
         {
+          TreeNode *goal_node = new TreeNode(goal_position, node);
           node->add_child(goal_node);
           node_list.push_back(goal_node);
           RCLCPP_INFO(get_logger(), "Path found");
+          publish_path();
           return;
         }
         std::vector<float> node_vec = {node->val[0] - random_position[0],
